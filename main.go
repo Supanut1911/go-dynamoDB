8b474a4b6bb56387a3fea1621218cc120143731f
Supanut1911 <ynotmela@gmail.com>
2023-09-13 11:44:31 +0700

    feature: add config load env

--- main.go
+++ main.go
@@ -1,5 +1,59 @@
 package main
 
-func main () {
-	print("hello go")
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/joho/godotenv"
+)
+
+
+type User struct {
+	name	string
+	age		int
+	job		string
 }
+
+func goDotEnvVariable(key string) string {
+
+  // load .env file
+  err := godotenv.Load(".env")
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  return os.Getenv(key)
+}
+
+func newClient(profile string ) (*dynamodb.Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("localhost"),
+		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+			func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: "http://localhost:8000"}, nil
+			})),
+		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID: goDotEnvVariable("AccessKeyID"), SecretAccessKey: goDotEnvVariable("SecretAccessKey"), SessionToken: "",
+				Source: "Mock credentials used above for local instance",
+			},
+		}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	c := dynamodb.NewFromConfig(cfg)
+	return c, nil
+}
+
+func main () {
+	print("hello go",goDotEnvVariable("AccessKeyID"),  goDotEnvVariable("SecretAccessKey"))
+}
+
